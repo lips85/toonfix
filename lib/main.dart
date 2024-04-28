3124f50a2c2fc0cb55a63877ac725c462fb05ff7
haru <godjaewook@naver.com>
2024-04-28 21:11:24 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Player {
+  String? name;
+
+  Player({required this.name});
+}
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello flutter!'),
+        ),
+        body: Center(
+          child: Text('Hello world!'),
+        ),
+      ),
+    );
+  }
+}
